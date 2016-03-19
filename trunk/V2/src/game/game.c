44817e69ccef3629903c410df81a11117b0d8704
amerisi <amerisi@U-BORDEAUX.FR>
2016-03-19 16:32:26 +0000

    Finalisation du module game, compilation vérifée

--- trunk/V2/src/game/game.c
+++ trunk/V2/src/game/game.c
@@ -22,25 +22,26 @@ struct game_s {
  **/
 game new_game (int width, int height, int nb_pieces, piece *pieces){
 
+	//verification of parameters
 	if(pieces == NULL || nb_pieces <= 0 || pieces[0] == NULL || width <= 0 || height <= 0)
 		return NULL;
 
-	game newGame = (game) malloc (sizeof (*newGame));
+	game new_g = (game) malloc (sizeof (*new_g));
 
-	if (newGame == NULL){
-		fprintf(stderr,"Problem in the alloc of newGame!!!");
+	if (new_g == NULL){
+		fprintf(stderr,"Problem in the allocation of newGame!!!");
 		exit(EXIT_FAILURE);
 	}
 
-	newGame->board = (int **) malloc (sizeof(*(newGame->board))*height);
+	new_g->board = (int **) malloc (sizeof(*(new_g->board))*height);
 
-	if (newGame->board == NULL){
-		fprintf(stderr, "Problem in the alloc of newGame's board");
+	if (new_g->board == NULL){
+		fprintf(stderr, "Problem in the allocation of newGame's board");
 		exit(EXIT_FAILURE);
 	}
 
 	for (int j= 0; j<height; ++j)
-		newGame->board[j] = (int *) malloc (sizeof(*(newGame->board[j]))*width);
+		new_g->board[j] = (int *) malloc (sizeof(*(new_g->board[j]))*width);
 
 	//look if piece in parameter don't intersect
 	for (int i= 0; i<nb_pieces -2; ++i){
@@ -52,15 +53,15 @@ game new_game (int width, int height, int nb_pieces, piece *pieces){
 		}
 	}
 
-	newGame->width = width;
-	newGame->height = height;
-	newGame->nb_move = 0;
-	newGame->nb_piece = 0;
+	new_g->width = width;
+	new_g->height = height;
+	new_g->nb_move = 0;
+	new_g->nb_piece = 0;
 
 	// initialization of the board
 	for (int w = 0; w<width; ++w )
 		for (int h = 0; h<height; ++h)
-			newGame->board[h][w] = -1;
+			new_g->board[h][w] = -1;
 
 	//course of the board to place the pieces
 	int x = 0;
@@ -68,44 +69,51 @@ game new_game (int width, int height, int nb_pieces, piece *pieces){
 	int w= 0;
 	int h = 0;
 
-	newGame->piece = malloc (nb_pieces * sizeof(*pieces));
-	if (newGame->piece == NULL){
-		fprintf(stderr,"Problem in alloc of piece in struct ");
+	new_g->piece = malloc (nb_pieces * sizeof(*pieces));
+	if (new_g->piece == NULL){
+		fprintf(stderr,"Problem in allocation of piece in structure ");
 		exit(EXIT_FAILURE);
 	}
 
-	for (; (newGame->nb_piece < nb_pieces) && (pieces[newGame->nb_piece] != NULL); ++(newGame->nb_piece)){
-		newGame->piece[newGame->nb_piece] = new_piece(pieces[newGame->nb_piece]);
+	for (; (new_g->nb_piece < nb_pieces) && (pieces[new_g->nb_piece] != NULL); ++(new_g->nb_piece)){
 
-		x = get_x((cpiece)pieces[newGame->nb_piece]);
-		y = get_y((cpiece)pieces[newGame->nb_piece]);
-		w = get_width((cpiece)pieces[newGame->nb_piece]);
-		h = get_height((cpiece)pieces[newGame->nb_piece]);
+		x = get_x((cpiece)pieces[new_g->nb_piece]);
+		y = get_y((cpiece)pieces[new_g->nb_piece]);
+		w = get_width((cpiece)pieces[new_g->nb_piece]);
+		h = get_height((cpiece)pieces[new_g->nb_piece]);
 
+		new_g->piece[new_g->nb_piece] = new_piece(x, y, w, h, can_move_x((cpiece)pieces[new_g->nb_piece]), can_move_y((cpiece)pieces[new_g->nb_piece]));
+
+		//verification piece totally on the board
 		if ( w==0 || h==0 || x+w >= width || y+h>= height){
 			fprintf (stderr, "Piece out of the size of the board");
 			exit(EXIT_FAILURE);
 		}
-		if(h == 1){ //in this case we know that the piece is on horizontal line
+
+		//in this case we know that the piece is on horizontal line
+		if(h == 1){
 			for (;x<w; ++x)
-				newGame->board[y][x] = newGame->nb_piece;
+				new_g->board[y][x] = new_g->nb_piece;
 		}
-		if (w == 1){ // in this case we know that the piece is on vertical line
+		// in this case we know that the piece is on vertical line
+		if (w == 1){
 			for(;y<h;++y)
-				newGame->board[y][x] = newGame->nb_piece;
+				new_g->board[y][x] = new_g->nb_piece;
 		}
-		else{ //in this case we know that the piece is at minimum a square 2*2
+		//in this case we know that the piece is at minimum a square 2*2
+		else{
 			for (;y<h;++y)
 				for(int tmp_x = x;tmp_x<w;++tmp_x)
-					newGame->board[y][tmp_x] = newGame->nb_piece;
+					new_g->board[y][tmp_x] = new_g->nb_piece;
 		}
 	}
-	if (newGame->nb_piece != nb_pieces){ //clean game if newGame->piece not corresponding to the nb_pieces of *pîece
-		delete_game(newGame); // free of all we allocted before
-		newGame = NULL;
+	//clean game if newGame->piece not corresponding to the nb_pieces of *piece
+	if (new_g->nb_piece != nb_pieces){
+		delete_game(new_g); // free of all we allocated before
+		new_g = NULL;
 	}
 
-  return newGame;
+  return new_g;
 
 }
 /**
@@ -117,14 +125,16 @@ game new_game_hr (int nb_piece, piece *piece)
 }
 
 void delete_game (game g){
-  for (int i = 0; i < g->nb_piece; ++i)
-    delete_piece(g->piece[i]);
-  free(g->piece);
-  for (int h = 0; h< g->height; ++h)
-	  free (g->board[h]);
-  free(g->board);
+	if (g != NULL){
+		for (int i = 0; i < g->nb_piece; ++i)
+			delete_piece(g->piece[i]);
+		free(g->piece);
+		for (int h = 0; h< g->height; ++h)
+			free (g->board[h]);
+		free(g->board);
 
-  free(g);
+		free(g);
+	}
 }
 
 void copy_game(cgame src, game dst){
@@ -140,7 +150,7 @@ void copy_game(cgame src, game dst){
       }
     }
     for (int i=0; i<src->nb_piece; ++i)
-      dst->piece[i] = src->piece[i];
+      copy_piece((cpiece)src->piece[i],dst->piece[i]);
 
     
   }
@@ -157,94 +167,48 @@ cpiece game_piece(cgame g, int piece_num){
 }
 
 bool game_over_hr(cgame g){
-  if( get_x(g->piece[0])==4 && get_y(g->piece[0])==3)
-    return true;
-  return false;
+  return get_x(g->piece[0])==4 && get_y(g->piece[0])==3;
 }
 
 //game_over_ar in module i don't know
 
 bool play_move(game g, int piece_num, dir d, int distance){
-  /*
+
   //piece_num on the board or not? 
-  if(piece_num >= g->nbPiece)
+  if(piece_num >= g->nb_piece && piece_num<0)
     return false;
   
-  //Test with the redCar first
-  else if(piece_num == 0){
-    //Direction correspond with the type of piece
-    if (d == UP || d == DOWN)
-      return false;
-    //copy the redCar to do the move on the copy
-    piece copyPiece = new_piece_rh(0,0,true,true);
-    copy_piece((cpiece)g->redCar, copyPiece);
+  int x = get_x(game_piece((cgame)g, piece_num));
+  int y = get_y(game_piece((cgame)g, piece_num));
+  int p_height = get_height(game_piece((cgame)g, piece_num));
+  int p_width = get_width(game_piece((cgame)g, piece_num));
+  int g_height = game_height((cgame)g);
+  int g_width = game_width((cgame)g);
 
-    //move of the copy 
-    move_piece(copyPiece,d,distance);
-
-    // on the board or not
-    if((get_x(copyPiece)<0 || get_x(copyPiece)>5) && (get_y(copyPiece)<0 || get_y(copyPiece)>5)){
-      delete_piece(copyPiece);
-      return false;
-    }
-
-    //copy not intersect with other pieces?
-    for(int i=0; i<g->nbPiece -2; ++i){
-      if(intersect((cpiece)copyPiece,(cpiece)(g->piece[i])))
-	{
-	  delete_piece(copyPiece);
-	  return false;
-	}
-    }
-    copy_piece((cpiece)copyPiece, g->redCar);
-    g->nbMove += d ;
-    delete_piece(copyPiece);
-    return true;
+  //Comparison of all the rectangle of the possible movement
+  if(can_move_y((cpiece)g->piece[piece_num]) && (d==UP || d==DOWN)){
+	  int new_h = p_height + distance;
+	  if (new_h>=0 && new_h<g_height){
+		  piece tmp_p = new_piece (x, y, p_width, new_h, false, false);
+		  if (!intersect(tmp_p, game_piece((cgame)g, piece_num))){
+			  move_piece(g->piece[piece_num],d,distance);
+			  g->nb_move +=d;
+			  return true;
+		  }
+	  }
   }
-  
-  //case of piece_num part of piece[]
-  else{
-    
-    //Test direction compatible
-    if(is_horizontal(g->piece[piece_num -1]) && (d==UP || d==DOWN))
-      return false;
-    else if(!is_horizontal(g->piece[piece_num -1]) && (d==LEFT || d==RIGHT))
-      return false;
-
-    //copy piece
-    piece copyPiece = new_piece_rh(0,0,true,true);
-    copy_piece(g->piece[piece_num],copyPiece);
-
-    //move on the copy
-    move_piece(copyPiece,d,distance);
-
-    //Test on the board
-    if((get_x(copyPiece)<0 || get_x(copyPiece)>5) && (get_y(copyPiece)<0 || get_y(copyPiece)>5)){
-      delete_piece(copyPiece);
-      return false;
-    }
-
-    //test intersect with other piece
-    if(intersect((cpiece)copyPiece, (cpiece)g->redCar))
-      return false;
-
-    //parcours de la partie gauche du tableau de piece du jeu autour de la piece a comparer
-    int i= 0;
-    for(;i!= piece_num - 1 ;++i)
-      if(intersect(copyPiece,g->piece[i]))
-	 return false;
-    i+=2;
-    //parcours de la partie droite du tableau de piece du jeu autour de la piece a comparer
-    for(;i!=g->nbPiece-2;++i)
-      if(intersect(copyPiece,g->piece[i]))
-	return false;
-
-    copy_piece((cpiece)copyPiece,g->piece[piece_num]);
-    g->nbMove += d;
-    return true;
-    }*/
+  if(can_move_x((cpiece)g->piece[piece_num]) && (d==LEFT || d==RIGHT)){
+	  int new_w = p_width + distance;
+	  if (new_w>=0 && new_w<g_width){
+		  piece tmp_p = new_piece (x, y, new_w, p_height, false, false);
+		  if (!intersect(tmp_p, game_piece((cgame)g, piece_num))){
+			  move_piece(g->piece[piece_num], d, distance);
+			  g->nb_move +=d;
+			  return true;
+		  }
+	  }
+  }
   return false;
-
 }
 
 int game_nb_moves(cgame g){
@@ -261,8 +225,4 @@ int game_height(cgame g){
 int game_square_piece (game g, int x, int y){
   return g->board[y][x];
 }
-game g, int x, int y){
-  if ( g->board[y][x] == -1)
-    return -1;
-  return g->board[y][x];
-}
+
