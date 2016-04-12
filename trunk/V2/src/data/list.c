6f7a5ae4c20e403b34655c4906e6c74432b33bed
amerisi <amerisi@U-BORDEAUX.FR>
2016-04-12 10:22:59 +0000

    ajout de toute modification du a la modification des prototype du .h de list et fin de l implémentation de chaque fonction du module

--- trunk/V2/src/data/list.c
+++ trunk/V2/src/data/list.c
@@ -1,5 +1,6 @@
 #include "list.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 struct list_s{
   void* data;
@@ -8,60 +9,85 @@ struct list_s{
 
 list create_list(void* data, list n_list)
 {
-  list list = malloc (sizeof(*list));
-  if(list!=NULL)
+  list _list = malloc (sizeof(*_list));
+  if(_list!=NULL)
   {
-    list->data = data;
-    list->next = n_list; 
+    _list->data = data;
+    _list->next = n_list; 
   }
-  return list;
+  return _list;
 }
 
-void* get_data(list list)
+void* get_data(list _list)
 {
-  return list->data;
+  return _list->data;
 }
 
-list next(list list)
+list next(list _list)
 {
-  return list->next;
+  return _list->next;
 }
 
-bool has_next(list list)
+bool has_next(list _list)
 {
-  return list->next!=NULL;
+  return _list->next!=NULL;
 }
 
-bool empty_list(list list)
+bool empty_list(list _list)
 {
-  return list==NULL;
+  return _list==NULL;
 }
 
-void add_after(list list, void* data)
+void add_after(list _list, void* data)
 {
-  while(!(empty_list(list)) && has_next(list))
-    list = list->next;
-  if (list)
-    list->next = create_list(data, NULL);
+  while(!(empty_list(_list)) && has_next(_list))
+    _list = _list->next;
+  if (_list)
+    _list->next = create_list(data, NULL);
 }
 
-void erase_first(list list)
+list erase_first(list _list)
 {
+  if (!has_next(_list)){
+    free(_list);
+    return NULL;
+  }
+  list first = _list ;
+  _list = _list->next;
+  free(first);
+  return _list;
   
 }
 
-void erase_after(list list)
+list erase_after(list _list, int index)
 {
-  
-}
-list prev =NULL;
-void delete_list(list list, void (*delete)(void *))
-{
-  while(!empty_list(list))
+  if (index==0)
+    return erase_first(_list);
+  list tmp = _list;
+  int current_index = 0;
+  while((!empty_list(tmp)) && current_index<=index)
   {
-    delete(list->data);
-    prev = list;
-    list = list->next;
+    tmp = next(tmp);
+    current_index++;
+  }
+  if(empty_list(tmp))
+    printf("Your index in parameters is out of length list");
+    return _list;
+  if(!has_next(tmp))
+     return _list;
+  list next_to_erase = next(tmp);
+  tmp->next = next_to_erase->next;
+  free(next_to_erase);    
+  return _list;
+}
+
+void delete_list(list _list, void (*delete)(void *))
+{
+  while(!empty_list(_list))
+  {
+    delete(_list->data);
+    list prev = _list;
+    _list = _list->next;
     free(prev);
   }
 }
