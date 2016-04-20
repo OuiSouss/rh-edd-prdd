479d2d050c73b82c4e7002d0bdd864d151f3ac4f
amerisi <amerisi@U-BORDEAUX.FR>
2016-04-20 14:50:14 +0000

    ajout de toutes les modifications sur le solveur et le CMakeLists pour creer solveur

--- trunk/V2/src/game/solve.c
+++ trunk/V2/src/game/solve.c
@@ -1,12 +1,13 @@
 #include "game.h"
 #include "piece.h"
 #include "list.h"
-
-#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define EXIT_LINE_RH 3
+
 /**
+ * @brief Permit to have the line where red car exit for a rush hour game
  * @pre length of line_exit need to be equals to the width of the game g
  **/
 
@@ -19,23 +20,34 @@ void piece_on_line_exit(game g, int* line_exit)
   }
 }
 
+/**
+ * @return true if the piece at position piece_num in t is marked
+ **/
 bool is_marked(bool *t, int piece_num)
 {
   return t[piece_num];
 }
 
-bool* mark_piece(int piece_num, bool* tab_marked)
+/**
+ * @brief Permit to mark a piece at position piece_num in the board array_marked
+ **/
+void mark_piece(int piece_num, bool* array_marked)
 {
-  tab_marked[piece_num] = true;
-  return tab_marked;
+  array_marked[piece_num] = true;
 }
 
-bool* unmark_piece(int piece_num, bool* tab_marked)
+/**
+ * @brief Permit to unmark a piece at position piece_num in the board array_marked
+**/
+void unmark_piece(int piece_num, bool* array_marked)
 {
-  tab_marked[piece_num] = false;
-  return tab_marked;
+  array_marked[piece_num] = false;
 }
 
+/**
+ * @brief Search the number of a piece that bloked the exit of red car
+ * @return the number of piece that is the nearest of the exit
+ **/
 int search_nearest_exit(int* line_exit, int width)
 {
   int i = 0;
@@ -49,6 +61,9 @@ int search_nearest_exit(int* line_exit, int width)
   return nb_piece_nearest_exit;
 }
 
+/**
+ * @return number of free space of the game in parameter
+ **/
 int number_free_spaces(cgame g)
 {
   int free_spaces = 0;
@@ -61,7 +76,11 @@ int number_free_spaces(cgame g)
   return free_spaces;
 }
 
-int solve_rush_hour(cgame g)
+/**
+ * @brief fonction which have the role to solve a game but it doesn't work
+ * @return number of move we done to solve game
+ **/
+int solve_rush_hour(game g)
 {
   if (number_free_spaces(g)==0)
     return -1;
@@ -76,6 +95,53 @@ int solve_rush_hour(cgame g)
   while (!game_over_hr(copy_g))
   {
     int current_p = search_nearest_exit(line_exit,width);
+    cpiece current_piece = game_piece(g,current_p);
   }
-  return 0;
+  return game_nb_moves((cgame)copy_g);
+}
+
+int main(int argc, char* argv[])
+{
+  if (argc==3)
+  {
+    if (atoi(argv[1])=='r')
+    {
+      FILE* file_solve = fopen(argv[2],"r");
+      if(file_solve==NULL)
+      {
+	fprintf(stderr, "Problem in openning file in argument\n");
+	return EXIT_FAILURE;
+      }
+      int width,height,nb_pieces = 0;
+      int x,y,p_width,p_height,move_x,move_y = 0;
+      piece *pieces;
+      
+      while(!feof(file_solve))
+      {
+	fscanf(file_solve,"%d %d",&width,&height);
+	fscanf(file_solve,"%d",&nb_pieces);
+	pieces = malloc(sizeof(*pieces)*nb_pieces);
+	if (pieces==NULL)
+	{
+	  fprintf(stderr,"Allocation problem");
+	  exit(EXIT_FAILURE);
+	}
+	int cmp = 0;
+	while(!feof(file_solve))
+	{
+	  fscanf(file_solve,"%d %d %d %d %d %d", &x,&y,&p_width,&p_height,&move_x,&move_y);
+	  pieces[cmp] = new_piece(x,y,p_width,p_height,move_x,move_y);
+	}
+      }
+      game g = new_game(width,height,nb_pieces,pieces);
+      solve_rush_hour(g);
+      fprintf(stdout, "Ceci est la mauvaise réponse, suite à un manque d'organisation le solveur n'est pas fini");
+
+      fclose(file_solve);
+    }
+    else if (atoi(argv[1])=='a')
+      printf("Nous sommes vraiment désolés mais notre algorithme ne fonctionne vraiment pas pour un ane rouge. Déjà, il faudrait qu'il fonctionne pour le rush hour!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+  }
+  else
+    fprintf(stderr, "Arguments are probably false: ./solveur  'a or r' config.txt\n");
 }
